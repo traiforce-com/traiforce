d42e25ac3343d0c4f819af524030f04d39489f18
syuumu <syuumu200@gmail.com>
2025-06-16 21:12:44 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-  index5
+    index5
     <h1 class="text-3xl font-bold mb-4">Welcome to trAIforce</h1>
+    <p>aaaaa</p>
     <p class="mb-4">This is the home page of your Nuxt.js application.</p>
-    <NuxtLink to="/about" class="text-blue-500 hover:underline">Go to About Page</NuxtLink>
-    <NuxtLink to="/info" class="text-blue-500 hover:underline ml-4">Go to Info Page</NuxtLink>
-    <NuxtLink to="/top" class="text-blue-500 hover:underline ml-4">Go to Top Page</NuxtLink>
+    <NuxtLink to="/about" class="text-blue-500 hover:underline"
+      >Go to About Page</NuxtLink
+    >
+    <NuxtLink to="/info" class="text-blue-500 hover:underline ml-4"
+      >Go to Info Page</NuxtLink
+    >
+    <NuxtLink to="/top" class="text-blue-500 hover:underline ml-4"
+      >Go to Top Page</NuxtLink
+    >
   </div>
 </template>
