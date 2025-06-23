0dd6822b969ee84eecd65eecf95dbc9674666251
syuumu <syuumu200@gmail.com>
2025-06-23 20:59:39 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <div></div>
+  <div class="mx-auto">
+    <div class="text-center mt-10">
+      <h1 class="text-5xl">３つの力で、世界を楽しくアップデートする。</h1>
+      <p class="mt-3">まずはお気軽にご相談ください。</p>
+    </div>
+  </div>
 </template>
