f5acdbb4a9d83b05e9411e51853314754903c747
syuumu <syuumu200@gmail.com>
2025-06-23 21:10:31 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="mx-auto">
     <div class="text-center mt-10">
-      <h1 class="text-5xl">３つの力で、世界を楽しくアップデートする。</h1>
-      <p class="mt-3">まずはお気軽にご相談ください。</p>
+      <h1 class="text-5xl">３つの力で、世界を<br />楽しくアップデートする</h1>
+      <p class="mt-3">アイデアがアイデアのままで終わる時代は、終わりました。</p>
+      <div class="grid grid-cols-3 gap-3 mt-10">
+        <div>
+          <p class="text-center text-4xl">Try</p>
+          <p>3人で力を合わせ、常識にとらわれない価値の創造へ挑戦を。</p>
+        </div>
+        <div>
+          <p class="text-center text-4xl">AI</p>
+          <p>最先端のAI技術を駆使し、想像を超えるソリューションを。</p>
+        </div>
+        <div>
+          <p class="text-center text-4xl">Force</p>
+          <p>私たちのサービスで、人々の心を動かし、社会にポジティブな影響力を。</p>
+        </div>
+      </div>
+      <p class="text-7xl font-bold mt-10">Tr<span class="text-red-400">AI</span>force</p>
     </div>
   </div>
 </template>
